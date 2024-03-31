95e76046bb53fc794c251176741ec3ecc8e23c9d
paipragathi <paipragathi007@gmail.com>
2024-03-31 17:55:12 +0530

    pages component rendered

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,4 +4,5 @@ import { signup } from "../controllers/auth.controller.js";
 const router= express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 export default router;
